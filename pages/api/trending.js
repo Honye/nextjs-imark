90f9487564a3fd81c01e38fcbce5b3c264a2d8f2
honyex <44310771+honyex@users.noreply.github.com>
2021-03-23 04:30:07 +0800

    Update trending.js

--- pages/api/trending.js
+++ pages/api/trending.js
@@ -1,31 +1,37 @@
-const fetchTrendingList = async () => {
-  const data = await fetch('https://github.com/trending');
-  const $ = cheerio.load(await data.text());
-  const list = $('.Box .Box-row')
-    .get()
-    .map(repo => {
-      const $repo = $(repo);
-      const title = $repo.find('.h3').text().trim();
-      const [owner, name] = title.split('/').map(v => v.trim());
-      const description = $(($repo.children())[2]).text().trim();
-      const language = $repo.find('[itemprop="programmingLanguage"]').text().trim();
-      const starCount = $repo.find('[aria-label="star"].octicon.octicon-star').parent().text().trim();
-      return {
-        owner: {
-          login: owner,
-          avatar_url: `https://github.com/${owner}.png`
-        },
-        name,
-        description,
-        language,
-        stargazers_count: starCount
-      };
+const fetchTrendingList = () => {
+  return fetch('https://github.com/trending')
+    .then((data) => {
+      data.text().then((text) => {
+        const $ = cheerio.load(text);
+        const list = $('.Box .Box-row')
+          .get()
+          .map(repo => {
+            const $repo = $(repo);
+            const title = $repo.find('.h3').text().trim();
+            const [owner, name] = title.split('/').map(v => v.trim());
+            const description = $(($repo.children())[2]).text().trim();
+            const language = $repo.find('[itemprop="programmingLanguage"]').text().trim();
+            const starCount = $repo.find('[aria-label="star"].octicon.octicon-star').parent().text().trim();
+            return {
+              owner: {
+                login: owner,
+                avatar_url: `https://github.com/${owner}.png`
+              },
+              name,
+              description,
+              language,
+              stargazers_count: starCount
+            };
+          });
+        return list;
+      });
     });
-  return list;
 };
 
-export default async (req, res) => {
-  const ret = await fetchTrendingList();
-  res.statusCode = 200;
-  res.json(ret);
+export default (req, res) => {
+  fetchTrendingList()
+    .then((data) => {
+      res.statusCode = 200;
+      res.json(data);
+    });
 }
