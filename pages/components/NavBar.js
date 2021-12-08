f6e56cae48f7443bee4f64c595daea51d133d68d
Honye <hongye.jun@qq.com>
2021-12-08 19:09:49 +0800

    feat: 增加顶部导航栏

---
+++ pages/components/NavBar.js
@@ -0,0 +1,88 @@
+import { useCallback } from 'react';
+import Link from 'next/link';
+
+const NavBar = () => {
+  const onGitHubLogin = useCallback(() => {
+    const searchParams = new URLSearchParams();
+    searchParams.append('client_id', process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID);
+    searchParams.append('scope', 'repo admin:repo_hook read:user');
+    searchParams.append('state', 'imark');
+    const { origin } = location;
+    searchParams.append('redirect_uri', `${origin}/api/authorize?redirect=${encodeURIComponent(location.href)}`);
+    location.href = `https://github.com/login/oauth/authorize?${searchParams.toString()}`;
+  }, []);
+
+  return (
+    <>
+      <header className="header">
+        <nav className="nav">
+          <Link href="/">
+            <a className="logo">
+              <h1>iMark</h1>
+            </a>
+          </Link>
+          <menu className="menu">
+            <li>
+              <Link href="/apis"><a className="menu-item">API</a></Link>
+            </li>
+            <li>
+              <Link href="/movies"><a className="menu-item">Movies</a></Link>
+            </li>
+          </menu>
+          <div className="user">
+            <button className="sign-in" onClick={onGitHubLogin}>Sign in</button>
+          </div>
+        </nav>
+      </header>
+      <style jsx>{`
+        .header {
+          background-color: rgba(246, 221, 102, 1);
+          padding: 14px 16px;
+        }
+        .nav {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+        }
+        .logo {
+          margin-right: 30px;
+          font-size: 1.5rem;
+          font-weight: bold;
+          font-style: italic;
+        }
+        .menu {
+          flex: auto;
+          display: flex;
+          align-items: center;
+          list-style: none;
+          margin: 0;
+          padding: 0;
+          gap: 16px;
+          font-weight: 500;
+        }
+        .menu-item {
+          font-size: 0.9rem;
+          padding: 4px 8px;
+          border-radius: 4px;
+        }
+        .menu-item:hover {
+          background-color: rgba(234, 76, 137, 0.1);
+        }
+        .sign-in {
+          display: inline-block;
+          padding: 6px 12px;
+          border-radius: 4px;
+          background-color: #000;
+          color: #fff;
+          font-size: 14px;
+        }
+        .sign-in:focus {
+          outline: none;
+          box-shadow: 0 0 0 4px rgba(234, 76, 137, 0.1)
+        }
+      `}</style>
+    </>
+  );
+};
+
+export default NavBar;
--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,70 @@
-import { useCallback } from 'react';
 import Head from 'next/head';
-import GitHubIcon from '../assets/github.svg';
+import NavBar from './components/NavBar';
 import styles from '../styles/Home.module.css';
 export default function Home() {
-  const onGitHubLogin = useCallback(() => {
-    const searchParams = new URLSearchParams();
-    searchParams.append('client_id', process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID);
-    searchParams.append('scope', 'repo admin:repo_hook read:user');
-    searchParams.append('state', 'imark');
-    const { origin } = location;
-    searchParams.append('redirect_uri', `${origin}/api/authorize?redirect=${encodeURIComponent(location.href)}`);
-    location.href = `https://github.com/login/oauth/authorize?${searchParams.toString()}`;
-  }, []);
-
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>iMark</title>
       </Head>
+      <NavBar />
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Welcome to <a href="https://nextjs.org">Next.js!</a>
+          </h1>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+          <p className={styles.description}>
+            Get started by editing{' '}
+            <code className={styles.code}>pages/index.js</code>
+          </p>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+          <div className={styles.grid}>
+            <a href="/search" className={styles.card}>
+              <h3>Browser &rarr;</h3>
+              <p>Search and access the input url.</p>
+            </a>
+            <a href="https://nextjs.org/docs" className={styles.card}>
+              <h3>Documentation &rarr;</h3>
+              <p>Find in-depth information about Next.js features and API.</p>
+            </a>
 
-        <div className={styles.grid}>
-          <a href="/search" className={styles.card}>
-            <h3>Browser &rarr;</h3>
-            <p>Search and access the input url.</p>
-          </a>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+            <a href="https://nextjs.org/learn" className={styles.card}>
+              <h3>Learn &rarr;</h3>
+              <p>Learn about Next.js in an interactive course with quizzes!</p>
+            </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+            <a
+              href="https://github.com/vercel/next.js/tree/master/examples"
+              className={styles.card}
+            >
+              <h3>Examples &rarr;</h3>
+              <p>Discover and deploy boilerplate example Next.js projects.</p>
+            </a>
 
+            <a
+              href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              className={styles.card}
+            >
+              <h3>Deploy &rarr;</h3>
+              <p>
+                Instantly deploy your Next.js site to a public URL with Vercel.
+              </p>
+            </a>
+          </div>
+        </main>
+
+        <footer className={styles.footer}>
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            Powered by{' '}
+            <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
           </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-        <button className={styles['login-btn']} onClick={onGitHubLogin}>
-          <GitHubIcon />GitHub
-        </button>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+        </footer>
+      </div>
+    </>
   )
 }
